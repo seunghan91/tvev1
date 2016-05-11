f8eeaecaca1b1ae5910ba12e38aba2b29e3fe0d3
seunghan Kim <theqwe2000@naver.com>
2016-05-11 04:34:48 +0000

    design

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  get ':controller(/:action(/:id))'
+  post ':controller(/:action(/:id))'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
